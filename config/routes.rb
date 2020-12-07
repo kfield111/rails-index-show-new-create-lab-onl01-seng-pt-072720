f86f9bebe5a34375d328d4ac515f10943be61871
Kelin Field <kfield111@hotmail.com>
2020-12-06 21:52:04 -0600

    Table creation working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :coupons, only: [:index, :new, :create, :show]
-
-  get '/coupon/new', to: 'coupons#new'
+  get '/coupons/:id/show', to: 'coupons#show'
+  get '/coupons/new', to: 'coupons#new'
 
 end
