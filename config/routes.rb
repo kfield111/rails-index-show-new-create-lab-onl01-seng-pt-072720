932a97d99be6bd0a0b364fd5ce9afba15fae0f64
Kelin Field <kfield111@hotmail.com>
2020-12-06 22:01:55 -0600

    commit tree 9ce644c9cffb90efa0328726da9a56acab93ed31

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   resources :coupons, only: [:index, :new, :create, :show]
-  get '/coupons/:id/show', to: 'coupons#show'
-  get '/coupons/new', to: 'coupons#new'
 
 end
